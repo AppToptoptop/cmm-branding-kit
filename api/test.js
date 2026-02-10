5b14acb927a31cfc1e2d409153d252ba17d8f56d
Aaron Perso <75019229+AppToptoptop@users.noreply.github.com>
2026-02-10 13:53:39 +0800

    Remove test endpoint, API is working

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export const config = { runtime: 'edge' };
-
-export default function handler() {
-  return new Response('Edge function works!', {
-    headers: { 'Content-Type': 'text/plain' },
-  });
-}
